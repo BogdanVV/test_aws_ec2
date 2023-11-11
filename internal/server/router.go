473c6e3729882e23adf8f2b679d4851f944a2c14
bogdanvv <bogdan.voinalovych@gmail.com>
2023-11-11 17:56:14 +0200

    feat: add more middleware

--- internal/server/router.go
+++ internal/server/router.go
@@ -10,7 +10,12 @@ import (
 
 func (*Server) InitRoutes() *chi.Mux {
 	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("qweasd"))
 	})
